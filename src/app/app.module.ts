d425c0616639c48044b4ccaba5114b91548b2a5a
Leigh Dinaya <leighdinaya04@gmail.com>
2017-03-21 21:02:30 +0800

    include angular2-flash-messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 import { FirebaseService } from './services/firebase.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -47,6 +48,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    FlashMessagesModule,
     AngularFireModule.initializeApp(firebaseConfig,firebaseAuthConfig),
     RouterModule.forRoot(appRoutes)
   ],
