d60d90324e37ce644cd3453759f7be457693b440
Leigh Dinaya <leighdinaya04@gmail.com>
2017-03-22 08:53:20 +0800

    attach login method to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private af: AngularFire, private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  login(){
+  	this.af.auth.login();
+  	this.flashMessage.show('You are now logged in.', {cssClass: 'alert-success', timeout: 3000});
   }
 
 }
