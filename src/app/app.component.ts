783a2994f73446a8c64c896716ebc0a0a3d0176c
Leigh Dinaya <leighdinaya04@gmail.com>
2017-03-23 16:49:41 +0800

    redirect to main page if not authenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+
+	constructor(
+		private auth: AngularFireAuth,
+		private router: Router){
+	}
+
+	ngOnInit(){
+		this.auth.subscribe(user=>{
+			if(!user){
+				this.router.navigate(['/']);
+			}
+		});
+	}
+
   title = 'app works!';
 }
