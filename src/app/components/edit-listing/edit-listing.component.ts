a8fe6ae7b65231fbeffd043835a4fd08e3395168
Leigh Dinaya <leighdinaya04@gmail.com>
2017-04-05 06:57:58 +0800

    get data from getListingDetails function to display in forms

--- src/app/components/edit-listing/edit-listing.component.ts
+++ src/app/components/edit-listing/edit-listing.component.ts
@@ -9,10 +9,41 @@ import { Router, ActivatedRoute, Params} from '@angular/router';
 })
 export class EditListingComponent implements OnInit {
 
+	id;
+	title;
+	bedrooms;
+	city;
+	owner;
+	type;
+	price;
+	image;
+
   constructor(private firebaseService: FirebaseService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-  	
+  	this.id = this.route.snapshot.params['id'];
+  	this.firebaseService.getListingDetails(this.id).subscribe(listing => {
+  		this.title = listing.title;
+  		this.owner = listing.owner;
+  		this.city = listing.city;
+  		this.bedrooms = listing.bedrooms;
+  		this.type = listing.type;
+  		this.price = listing.price;
+  		// todo add the image
+  	});
+  }
+
+  onEditSubmit(){
+  	let listing = {
+  		title: this.title,
+  		owner: this.owner,
+  		city: this.city,
+  		type: this.type,
+  		bedrooms: this.bedrooms,
+  		price: this.price
+  	}
+  	this.firebaseService.updateListing(this.id, listing);
+  	this.router.navigate(['/listings']);
   }
 
 }
